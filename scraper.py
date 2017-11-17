d5c7cddd1471948c7de0aa2e8e7aaf9f72203816
Maximillian Tinati <mrt59@cornell.edu>
2017-11-17 13:46:55 -0500

    added dummy scraping script

---
+++ scraper.py
@@ -0,0 +1,16 @@
+#scraper.py
+#November 15, 2017
+#Claire Tinati
+"""Script to scrape magiccards.info to get all prices of cards in each set and display them."""
+
+import requests
+
+
+
+
+def main():
+    getPage
+
+
+if __name__ == "__main__":
+    main()
